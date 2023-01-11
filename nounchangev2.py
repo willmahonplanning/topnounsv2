fd098bc17d9a5099b79df874b7452b17c70313eb
willmahonplanning <122417078+willmahonplanning@users.noreply.github.com>
2023-01-11 13:01:28 -0800

    Update nounchangev2.py

--- nounchangev2.py
+++ nounchangev2.py
@@ -1,5 +1,5 @@
 import spacy
-import en_core_web_sm
+nlp = spacy.load("en_core_web_sm")
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -8,7 +8,6 @@ app = Flask(__name__)
 def noun_detector():
     if request.method == "POST":
         input_string = request.form["input_string"]
-        nlp = spacy.load("en_core_web_sm")
         doc = nlp(input_string)
         modified_string = " ".join("top " + token.text if token.pos_ == "NOUN" else token.text for token in doc)
         return render_template("nounchangev2.html", modified_string=modified_string)
