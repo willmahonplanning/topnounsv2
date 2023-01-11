2b12eb7e436907a3ccbc6ac2f6802ab96a3ac482
willmahonplanning <122417078+willmahonplanning@users.noreply.github.com>
2023-01-11 11:06:39 -0800

    Update nounchangev2.py

--- nounchangev2.py
+++ nounchangev2.py
@@ -1,32 +1,17 @@
-from flask import Flask, request, render_template
-from pynlpi import NLPI
+import spacy
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-nlpi = NLPI()
 
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-@app.route("/noun_detector", methods=["POST"])
+@app.route("/", methods=["GET", "POST"])
 def noun_detector():
-    input_text = request.form.get("input_text")
-    if input_text:
-        # tokenize the input text
-        sentences = nlpi.sentence_tokenize(input_text)
-        
-        # perform part-of-speech tagging on the input text
-        pos_tags = nlpi.pos_tag(sentences)
-        
-        # find all nouns in the input text
-        nouns = []
-        for sentence in pos_tags:
-            for token, pos_tag in sentence:
-                if pos_tag.startswith("NN"):
-                    nouns.append(token)
-        return " ".join(nouns)
-    else:
-        return "Please enter some text"
-        
+    if request.method == "POST":
+        input_string = request.form["input_string"]
+        nlp = spacy.load("en_core_web_sm")
+        doc = nlp(input_string)
+        modified_string = " ".join("top " + token.text if token.pos_ == "NOUN" else token.text for token in doc)
+        return render_template("nounchangev2.html", modified_string=modified_string)
+    return render_template("nounchangev2.html")
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
